9e3a8756440233f62f5a1d3ef52e9ff5742901bd
JefferVentu <jvruiz62@gmail.com>
2025-01-23 14:23:17 -0500

    creacion de pantallas

--- components/drawer/CambioAlmacen.js
+++ components/drawer/CambioAlmacen.js
@@ -1,6 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+// colorfondo: #00c6d1
+// colorbotones: #043256
+// 2docolor: #f9be00
+// 3ercolor: #cccccc
+
 const CambioAlmacen = () => {
     return (
         <View style={styles.container}>
